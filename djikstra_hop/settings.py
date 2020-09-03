f04fdeac864edbc7a994fd941db5d13e07d3553a
Fernanda Murillo <fmurillo@alumni.cmu.edu>
2020-09-03 19:02:41 -0400

    Basic homepage

--- djikstra_hop/settings.py
+++ djikstra_hop/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'navigate.apps.NavigateConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
