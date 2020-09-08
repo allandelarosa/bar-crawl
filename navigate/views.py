4abab3853badc2baa92af938652fcd985de7e445
Fernanda Murillo <fmurillo@alumni.cmu.edu>
2020-09-08 17:04:04 -0400

    add search bar

--- navigate/views.py
+++ navigate/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def hop(request):
+	return render(request, 'navigate/hop.html')
+
 def home(request):
 	return render(request, 'navigate/home.html')
