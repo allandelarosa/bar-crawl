0cee07ba1abe371bdac5eea9c81ed0428efa08b7
Fernanda Murillo <fmurillo@alumni.cmu.edu>
2020-09-08 18:51:16 -0400

    updated settings

--- djikstra_hop/settings.py
+++ djikstra_hop/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=^hrl9xq25i4xv6e7p#=n811vyesw^bg!l5wnkm62vp=6gnld8'
+# SECRET_KEY = '=^hrl9xq25i4xv6e7p#=n811vyesw^bg!l5wnkm62vp=6gnld8'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djikstrahopapp.herokuapp.com']
 
 
 # Application definition
