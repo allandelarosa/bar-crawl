22d8575421548152b12c9b4ba8fd39e6a2d7ace9
Allan Delarosa <ajd2215@columbia.edu>
2020-09-15 19:26:06 -0400

    can search for location, but only one result

--- navigate/views.py
+++ navigate/views.py
@@ -4,7 +4,11 @@ from django.http import HttpResponse
 from django.conf import settings
 
 def hop(request):
-	return render(request, 'navigate/hop.html', {'API_KEY': settings.API_KEY})
+	context = {
+		'API_KEY': settings.API_KEY,
+	}
+
+	return render(request, 'navigate/hop.html', context)
 
 def home(request):
 	return render(request, 'navigate/home.html')
