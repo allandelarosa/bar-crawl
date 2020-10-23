64c77de9eb4ea6b4a4c6965c615b336215707213
Allan Delarosa <ajd2215@columbia.edu>
2020-10-23 14:56:43 -0400

    First version of algorithm to make graph

--- navigate/views.py
+++ navigate/views.py
@@ -9,6 +9,8 @@ import json
 import math
 import heapq
 
+from collections import defaultdict
+
 def hop(request):
     context = {
         'API_KEY': settings.API_KEY,
@@ -47,7 +49,7 @@ def construct_graph(request):
 		A = math.sin(dLat/2) * math.sin(dLat/2) + math.cos(math.radians(a["lat"])) * math.cos(math.radians(b["lat"])) * math.sin(dLng/2) * math.sin(dLng/2)
 		C = 2 * math.atan2(math.sqrt(A), math.sqrt(1-A))
 		D = R * C # Distance in km
-		return D
+		return D + 0.001
 
 	class DSU:
 		def __init__(self, names):
@@ -67,6 +69,7 @@ def construct_graph(request):
 	edges = []
 	for i, loc1 in enumerate(location_data):
 		for loc2 in location_data[i + 1:]:
+			print(distance(loc1, loc2))
 			heapq.heappush(
 				edges,
 				(distance(loc1, loc2), loc1, loc2)
@@ -74,9 +77,15 @@ def construct_graph(request):
 
 	seen = set()
 	graph = []
+	rejected = []
+
+	min_edges = {}
+	# used_edges = defaultdict(list)
+
 	while edges:
 		dist, loc1, loc2 = heapq.heappop(edges)
 		if dsu.find(loc1["name"]) == dsu.find(loc2["name"]):
+			rejected.append((dist, loc1, loc2))
 			continue
 		dsu.union(loc1["name"], loc2["name"])
 
@@ -88,8 +97,37 @@ def construct_graph(request):
 		seen.add(loc1["name"])
 		seen.add(loc2["name"])
 
+		if loc1["name"] not in min_edges:
+			min_edges[loc1["name"]] = dist
+		else:
+			min_edges[loc1["name"]] = min(dist, min_edges[loc1["name"]])
+
+		if loc2["name"] not in min_edges:
+			min_edges[loc2["name"]] = dist
+		else:
+			min_edges[loc2["name"]] = min(dist, min_edges[loc2["name"]])
+
+		# used_edges[loc1["name"]].append(dist)
+		# used_edges[loc2["name"]].append(dist)
+
 		pars = set(par for par in dsu.par.values())
 		if len(pars) == 1:
 			break
+
+	# def median(x):
+	# 	return x[len(x)//2]
+
+	WEIGHT = 1.5
+
+	for dist, loc1, loc2 in rejected:
+		# if dist <= WEIGHT * (min_edges[loc1["name"]] + min_edges[loc2["name"]]) / 2:
+		if dist <= WEIGHT * min_edges[loc1["name"]] or dist <= WEIGHT * min_edges[loc2["name"]]:
+		# if dist <= WEIGHT * min(median(used_edges[loc1["name"]]), median(used_edges[loc2["name"]])):
+			graph.append([
+				{"lat": loc1["lat"], "lng": loc1["lng"]},
+				{"lat": loc2["lat"], "lng": loc2["lng"]}
+				])
+			# heapq.heappush(used_edges[loc1["name"]], dist)
+			# heapq.heappush(used_edges[loc2["name"]], dist)
 	
 	return JsonResponse(graph, safe=False)
