42ab6e2d9fb2cc895060eba0a3de202eeb84e7c1
Fernanda Murillo <fmurillo@alumni.cmu.edu>
2020-11-10 18:39:12 -0500

    bar pic background

--- navigate/static/navigate/js/home.js
+++ navigate/static/navigate/js/home.js
@@ -6,3 +6,12 @@ function initMap() {
         window.location.href = "/hop/" + input.value;
     });
 }
+
+
+// let background_div = document.createElement('div');
+// var elem = document.createElement("img");
+// elem.src = "/static/navigate/images/barpic.jpeg";
+// // document.getElementById("placehere").appendChild(elem);
+// background_div.appendChild(elem)
+// background_div.classList.add('barpic_background')
+
